3ce5878413021da16c856198e4171e61fd3feab9
PetrNiki <nikitenkopv@yandex.ru>
2022-07-10 07:27:21 +0300

    Задача1

---
+++ Задача1/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int num = number % 10;
+Console.WriteLine("Единицы: " + num);
